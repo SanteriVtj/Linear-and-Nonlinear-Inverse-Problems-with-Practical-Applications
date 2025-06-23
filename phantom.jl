214f112fbd78219030f78564795b58c03e5d2ec1
SanteriVtj <santeri.vaataja@gmai.com>
2025-06-23 16:31:03 +0300

    Three circle example with large N

--- phantom.jl
+++ phantom.jl
@@ -4,8 +4,8 @@ function dist(x::Base.AbstractCartesianIndex, y::Base.AbstractCartesianIndex)
     return norm(x.I.-y.I)
 end
 
-function three_circles(;n=50, centers=[CartesianIndex(25,10), CartesianIndex(25,25), CartesianIndex(25,40)], R=[7,3,7],values=[.6,.7,.8])
-    @assert length(centers) == length(R)
+function circles(;n=50, centers=[CartesianIndex(25,10), CartesianIndex(25,25), CartesianIndex(25,40)], R=[7,3,7],values=[.6,.7,.8])
+    @assert length(centers) == length(R) == length(values)
     X = zeros(n,n)
     indices = findall(x->true, X)
     for (rind,i) in enumerate(centers)
@@ -29,7 +29,7 @@ bp = RadonKA.backproject(R, θ)
 heatmap(bp)
 
 
-tc = three_circles()
+tc = circles()
 heatmap(tc)
 
 t = [0, π/4, π/2, 3π/2, π]
@@ -37,3 +37,19 @@ tcRadon = RadonKA.radon(tc, θ)
 heatmap(tcRadon)
 tcBackRadon = RadonKA.backproject(tcRadon, θ)
 heatmap(tcBackRadon)
+
+tcLarge = circles(
+    n = 500,
+    centers = [
+        CartesianIndex(250,100),
+        CartesianIndex(250,250),
+        CartesianIndex(250,400),
+    ],
+    R = [70,30,70]
+)
+heatmap(tcLarge)
+
+tcLargeRadon = RadonKA.radon(tcLarge, θ)
+heatmap(tcLargeRadon)
+tcLargeBackRadon = RadonKA.backproject(tcLargeRadon, θ)
+heatmap(tcLargeBackRadon)
